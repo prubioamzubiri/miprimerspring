d7a64339904126a2e083e78d60ba6fc601085076
Pablo Rubio <prubioam@zubirimanteo.com>
2024-09-11 09:41:41 +0200

    Add initial project files

---
+++ src/main/java/com/zubiri/miprimerspring/MiprimerspringApplication.java
@@ -0,0 +1,13 @@
+package com.zubiri.miprimerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiprimerspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiprimerspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zubiri/miprimerspring/MiprimerspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubiri.miprimerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiprimerspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
