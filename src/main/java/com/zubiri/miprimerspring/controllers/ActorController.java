474fdc3f96dec9c7206bdb9a1bb9ca129a395e08
Pablo Rubio <prubioam@zubirimanteo.com>
2024-10-07 11:23:11 +0200

    Refactor ActorController to add endpoint for retrieving an actor by ID

--- src/main/java/com/zubiri/miprimerspring/controllers/ActorController.java
+++ src/main/java/com/zubiri/miprimerspring/controllers/ActorController.java
@@ -1,5 +1,26 @@
 package com.zubiri.miprimerspring.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zubiri.miprimerspring.aplicacion.IAplicacion;
+import com.zubiri.miprimerspring.dominio.Actor;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/actor")
+@AllArgsConstructor
 public class ActorController {
+
+    IAplicacion<Actor> aplicacionActores;
+
+    @GetMapping("/{id}")
+    public Actor buscarActor(@PathVariable Integer id)
+    {
+        return aplicacionActores.buscar(id);
+    }
     
 }
