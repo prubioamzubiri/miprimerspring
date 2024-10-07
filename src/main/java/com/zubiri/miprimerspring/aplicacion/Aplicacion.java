092d564a536a049ea3f734aa45ce68c023a9f698
Pablo Rubio <prubioam@zubirimanteo.com>
2024-10-07 11:17:35 +0200

    Refactor guardar method in IAplicacion interface to return boolean

---
+++ src/main/java/com/zubiri/miprimerspring/aplicacion/Aplicacion.java
@@ -0,0 +1,47 @@
+package com.zubiri.miprimerspring.aplicacion;
+
+import com.zubiri.miprimerspring.persistencia.IPersistencia;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Aplicacion<T> implements IAplicacion<T> {
+
+    IPersistencia<T> persistencia;
+
+    @Override
+    public boolean guardar(T t) {
+        
+        try{
+            persistencia.guardar(t);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+        
+    }
+
+
+    @Override
+    public T buscar(Object id) {
+        
+        return persistencia.obtener(id);
+    }
+
+    @Override
+    public T buscarPorNombre(String name) {
+        return null;
+    }
+
+    @Override
+    public boolean eliminar(Object id) {
+        return false;
+    }
+
+    @Override
+    public java.util.List<T> obtenerTodos() {
+        return null;
+    }
+    
+}
