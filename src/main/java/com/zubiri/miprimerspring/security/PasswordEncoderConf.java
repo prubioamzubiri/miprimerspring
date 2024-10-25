5720b6bbc26c11f68f304a130da62a777033de99
prubioamzubiri <prubioam@zubirimanteo.com>
2024-10-25 13:05:11 +0200

    Refactor PasswordEncoderConf to use BCryptPasswordEncoder

--- src/main/java/com/zubiri/miprimerspring/security/PasswordEncoderConf.java
+++ src/main/java/com/zubiri/miprimerspring/security/PasswordEncoderConf.java
@@ -2,6 +2,7 @@ package com.zubiri.miprimerspring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,7 +12,7 @@ public class PasswordEncoderConf {
     
     @Bean
     public PasswordEncoder passwordEncoder() {
-        //return new BCryptPasswordEncoder();
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+        //return NoOpPasswordEncoder.getInstance();
     }
 }
