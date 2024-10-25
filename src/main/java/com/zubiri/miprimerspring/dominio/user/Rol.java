f1b626cd6122ea48b32d388b442dd67be8907fe7
Pablo Rubio <prubioam@zubirimanteo.com>
2024-10-25 21:16:53 +0200

    Refactorizar clases Rol y Usuario en el paquete dominio.user

--- src/main/java/com/zubiri/miprimerspring/dominio/user/Rol.java
+++ src/main/java/com/zubiri/miprimerspring/dominio/user/Rol.java
@@ -1,16 +1,5 @@
 package com.zubiri.miprimerspring.dominio.user;
 
-import org.springframework.security.core.GrantedAuthority;
-
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 public enum Rol{
     
     USER, ADMIN
--- src/main/java/com/zubiri/miprimerspring/dominio/user/Usuario.java
+++ src/main/java/com/zubiri/miprimerspring/dominio/user/Usuario.java
@@ -1,9 +1,7 @@
 package com.zubiri.miprimerspring.dominio.user;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
