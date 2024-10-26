1041b92c434ea025afd8fe235766b1c6cc412b7f
Pablo Rubio <prubioam@zubirimanteo.com>
2024-10-26 13:02:36 +0200

    Refactorizar SecurityConf.java para eliminar importaciones innecesarias y ajustar formato

--- src/main/java/com/zubiri/miprimerspring/security/SecurityConf.java
+++ src/main/java/com/zubiri/miprimerspring/security/SecurityConf.java
@@ -10,18 +10,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.zubiri.miprimerspring.persistencia.RepositorioUsuario;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
