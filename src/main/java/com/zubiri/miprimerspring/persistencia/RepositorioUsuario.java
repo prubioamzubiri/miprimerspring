348fbe8a664cb4b76930fb73338b5988bb3571f2
Pablo Rubio <prubioam@zubirimanteo.com>
2024-10-28 10:42:44 +0100

    Add RepositorioUsuario interface for managing user persistence

---
+++ src/main/java/com/zubiri/miprimerspring/persistencia/RepositorioUsuario.java
@@ -0,0 +1,12 @@
+package com.zubiri.miprimerspring.persistencia;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+import com.zubiri.miprimerspring.dominio.usuario.Usuario;
+
+public interface RepositorioUsuario extends CrudRepository<Usuario, Long> {
+    
+    Optional<Usuario> findByUsername(String username);
+    
+}
